397ef055704dac3315ab2f6e6456b32cd775bf68
Dimitri Dolgov <dad7648@rit.edu>
2020-12-30 12:15:51 -0500

    initial esmodule script

---
+++ shimmeringreach/shimmeringreach.js
@@ -0,0 +1,18 @@
+/**
+ * The Shimmering Reach Fantasy Tabletop System for use in Foundry VTT
+ * A system for playing the fifth edition of the worlds most popular roleplaying game.
+ * Author: Dimitri Dolgov
+ * Software License: TBD
+ * Content License: TBD
+ * Repository: https://github.com/Zgrill2/ShimmeringReach-FVTT
+ * Issue Tracker: https://github.com/Zgrill2/ShimmeringReach-FVTT/issues
+ */
+
+// IMPORT STATEMENTS
+import { RollDP } from "./module/dice-roller/roll.js";
+
+// HOOKS
+
+Hooks.once("init", function() {
+  console.log(`ShimmeringReach - Initializing`);
+});
